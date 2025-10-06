6eb4eaf8c6d7e7d01fe17737026b0ede0458024c
Your Name <you@example.com>
2025-10-06 07:52:14 +0300

    new index

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('',index_view),
     path("53c967b18b28db4c853278a5ccaa60ba/<int:pk>/", detail_view, name="detail"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def detail_view(request, pk):
     return render(request, "detail.html", {"pk": pk})
+
+def index_view(request):
+    return render(request, "index.html")
