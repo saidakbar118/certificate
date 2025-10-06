dfabb10dfa7396d8091eaa30a923d7d767cec7a0
Your Name <you@example.com>
2025-10-06 09:20:08 +0300

    collected

--- Admin/settings.py
+++ Admin/settings.py
@@ -116,10 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+import os
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[BASE_DIR/'static/']
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [BASE_DIR/'static']
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 LOGIN_URL = 'main:login'
 
